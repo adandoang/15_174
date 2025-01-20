f0026db6db697f41109ec08c7770d4469468a637
adandoang <akhdanjzif@gmail.com>
2025-01-20 21:52:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/monitoring_hewan/model/Hewan.kt
@@ -0,0 +1,26 @@
+package com.example.monitoring_hewan.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Hewan (
+    val id_hewan: String,
+    val nama_hewan: String,
+    val tipe_pakan: String,
+    val populasi: String,
+    val zona_wilayah: String
+)
+
+@Serializable
+data class AllHewanResponse(
+    val status: Boolean,
+    val messag: String,
+    val data:List<Hewan>
+)
+
+@Serializable
+data class HewanDetailResponse(
+    val status: Boolean,
+    val messag: String,
+    val data: Hewan
+)
