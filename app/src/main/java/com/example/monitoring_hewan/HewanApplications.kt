24eef0f521e7be1eae040c510d6d58184b8f45fa
adandoang <akhdanjzif@gmail.com>
2025-01-23 20:58:16 +0700

    membuat HewanApplications, HewanContainer.kt, TopAppBar.kt

--- app/src/main/java/com/example/monitoring_hewan/HewanApplications.kt
+++ app/src/main/java/com/example/monitoring_hewan/HewanApplications.kt
@@ -1,4 +1,13 @@
 package com.example.monitoring_hewan
 
-class HewanApplications {
+import android.app.Application
+import com.example.monitoring_hewan.dependenciesinjection.AppContainer
+import com.example.monitoring_hewan.dependenciesinjection.HewanContainer
+
+class HewanApplications: Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container= HewanContainer()
+    }
 }
--- app/src/main/java/com/example/monitoring_hewan/dependenciesinjection/HewanContainer.kt
+++ app/src/main/java/com/example/monitoring_hewan/dependenciesinjection/HewanContainer.kt
@@ -1,25 +1,28 @@
 package com.example.monitoring_hewan.dependenciesinjection
 
+import com.example.monitoring_hewan.repository.HewanRepository
+import com.example.monitoring_hewan.repository.NetworkHewanRepository
+import com.example.monitoring_hewan.service.HewanService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
 interface AppContainer{
-    val mahasiswaRepository: MahasiswaRepository
+    val hewanRepository: HewanRepository
 }
 
-class MahasiswaContainer : AppContainer {
-    private val baseUrl = "http://10.0.2.2:3000/api/mahasiswa/"
+class HewanContainer : AppContainer {
+    private val baseUrl = "http://10.0.2.2:3000/api/hewan/"
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl).build()
 
-    private val mahasiswaService: MahasiswaService by lazy {
-        retrofit.create(MahasiswaService::class.java)
+    private val hewanService: HewanService by lazy {
+        retrofit.create(hewanService::class.java)
     }
-    override val mahasiswaRepository: MahasiswaRepository by lazy {
-        NetworkMahasiswaRepository(mahasiswaService)
+    override val hewanRepository: HewanRepository by lazy {
+        NetworkHewanRepository(hewanService)
     }
 }
