b10f17381d7e3ee6a2be86995a1c2f394827fa58
adandoang <akhdanjzif@gmail.com>
2025-01-25 16:20:57 +0700

    membuat home view monitoring

---
+++ app/src/main/java/com/example/monitoring_hewan/view/MonitoringView/HomeMonitoringView.kt
@@ -0,0 +1,227 @@
+package com.example.monitoring_hewan.view.MonitoringView
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.monitoring_hewan.R
+import com.example.monitoring_hewan.customwidget.CostumeTopAppBar
+import com.example.monitoring_hewan.model.Monitoring
+import com.example.monitoring_hewan.navigation.DestinasiNavigasi
+import com.example.monitoring_hewan.viewmodel.PenyediaViewModel
+import com.example.monitoring_hewan.viewmodel.monitoringvm.HomeMonitoringViewModel
+import com.example.monitoring_hewan.viewmodel.monitoringvm.HomeUiState
+
+object DestinasiHomeMonitoring : DestinasiNavigasi {
+    override val route = "homemonitoring"
+    override val titleRes = "Monitoring"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenMonitoring(
+    navigateBack: ()-> Unit,
+    navigateToItemEntry: ()-> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit={},
+    viewModel: HomeMonitoringViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHomeMonitoring.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMtr()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add monitoring")
+            }
+        }
+    ){ innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.mtrUIState,
+            retryAction = { viewModel.getMtr() }, modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteMtr(it.id_monitoring)
+                viewModel.getMtr()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Monitoring) -> Unit={},
+    onDetailClick: (String) -> Unit
+){
+    when(homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.monitoring.isEmpty()) {
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Monitoring")
+                }
+            } else {
+                MtrLayout(
+                    monitoring = homeUiState.monitoring, modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.id_monitoring)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+//Homescreen menampilkan loading message
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+//Homescreen menampilkan error message
+@Composable
+fun OnError(retryAction: ()->Unit, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed),modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun MtrLayout(
+    monitoring: List<Monitoring>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Monitoring) -> Unit,
+    onDeleteClick: (Monitoring) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(monitoring){monitoring ->
+            MtrCard(
+                monitoring = monitoring,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(monitoring) },
+                onDeleteClick = {
+                    onDeleteClick(monitoring)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun MtrCard(
+    monitoring: Monitoring,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Monitoring) -> Unit ={}
+){
+    Card (
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ){
+        Column (
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = monitoring.id_monitoring,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick =  { onDeleteClick(monitoring)}) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
+                Text(
+                    text = monitoring.id_kandang,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = monitoring.tanggal_monitoring,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = monitoring.status,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
